2c6ab2dd524868ccb725e4560a974067af87cdca
paynesc1 <paynesc1@gmail.com>
2025-01-19 11:32:16 -0700

    Initial Commit

---
+++ leet.py
@@ -0,0 +1,12 @@
+# class Solution:
+#     def twoSum(self, data, target):
+#         for i in range(len(data)):
+#             for j in range(i+1, len(data)):
+#                 if data[i] + data[j] == target:
+#                     return [i,j]
+
+class Solution:
+    def twoSum(self, data, target):
+        for i in range of len(data):
+            print i
+            
