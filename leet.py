146e734e132f43ca39cf270a59653ee5f5f27525
paynesc1 <paynesc1@gmail.com>
2025-01-20 12:07:21 -0700

    Second Commit

--- leet.py
+++ leet.py
@@ -7,6 +7,13 @@
 
 class Solution:
     def twoSum(self, data, target):
-        for i in range of len(data):
-            print i
+        for i in range(len(data)):
+            for j in range(i+1, len(data)):
+                if data[i] + data[j] == target:
+                    return[i,j]
+solution = Solution()
+print(solution.twoSum([3,2,4], 6))
+#for value in list, starting at zero
+#add two value togeth to get target
+#3 add 
             
