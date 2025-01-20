cac3a4c6f8afdb34d5f5b9916dd16c644e609ccb
paynesc1 <paynesc1@gmail.com>
2025-01-20 12:11:09 -0700

    added another problem set to test

--- leet.py
+++ leet.py
@@ -1,19 +1,11 @@
-# class Solution:
-#     def twoSum(self, data, target):
-#         for i in range(len(data)):
-#             for j in range(i+1, len(data)):
-#                 if data[i] + data[j] == target:
-#                     return [i,j]
-
 class Solution:
     def twoSum(self, data, target):
         for i in range(len(data)):
             for j in range(i+1, len(data)):
                 if data[i] + data[j] == target:
                     return[i,j]
+                
 solution = Solution()
-print(solution.twoSum([3,2,4], 6))
-#for value in list, starting at zero
-#add two value togeth to get target
-#3 add 
+# print(solution.twoSum([3,2,4], 6))
+print(solution.twoSum([2,7,11,15], 9))
             
