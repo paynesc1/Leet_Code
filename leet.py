76023c2b4fa796e457a5d6305856eed206f66ace
paynesc1 <paynesc1@gmail.com>
2025-01-21 22:21:15 -0700

    Initial Commit for Problem 2

--- leet.py
+++ leet.py
@@ -10,16 +10,30 @@
 # # print(solution.twoSum([3,2,4], 6))
 # print(solution.twoSum([2,7,11,15], 9))
 
+
+""" *** Improved Time Complexity *** """
 # Improved time complexity using enumerate()
-class Solution:
-    def twoSum(self, data, target):
-        lookup = {}
-        for i, num in enumerate(data):
-            final = target - num
-            if final in lookup:
-                return[lookup[final], i]
-            lookup[num] = i
+# class Solution:
+#     def twoSum(self, data, target):
+#         lookup = {}
+#         for i, num in enumerate(data):
+#             final = target - num
+#             if final in lookup:
+#                 return[lookup[final], i]
+#             lookup[num] = i
                 
-solution = Solution()
-# print(solution.twoSum([2,7,11,15], 9))
-print(solution.twoSum([3,3],6))
+# solution = Solution()
+# # print(solution.twoSum([2,7,11,15], 9))
+# print(solution.twoSum([3,3],6))
+
+
+
+""" *** Leet Code Problem 2 *** """
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        
