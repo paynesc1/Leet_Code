b63c27b0c380d6393d9d7a4caac0adee25486a9d
paynesc1 <paynesc1@gmail.com>
2025-01-21 22:36:20 -0700

    Added notes

--- leet.py
+++ leet.py
@@ -30,10 +30,11 @@
 
 """ *** Leet Code Problem 2 *** """
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         
