baf17df6c968f31839698777651cf734b4383740
paynesc1 <paynesc1@gmail.com>
2025-01-21 22:15:10 -0700

    added problem set

--- leet.py
+++ leet.py
@@ -20,4 +20,5 @@ class Solution:
             lookup[num] = i
                 
 solution = Solution()
-print(solution.twoSum([2,7,11,15], 9))
+# print(solution.twoSum([2,7,11,15], 9))
+print(solution.twoSum([3,3],6))
